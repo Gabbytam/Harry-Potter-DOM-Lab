e7e2773d08afe78b578f064090148f1a985aea9b
Gabbytam <gtam2@fordham.edu>
2020-09-08 16:07:49 -0700

    Year 1 - added h1 using javascript

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log('DOM is loaded!')
 })
+
+const container= document.getElementById('container');
+console.log(container);
+
+const h1= document.createElement('h1');
+console.log(h1);
+h1.innerText= 'Hogwarts';
+//document.body.appendChild(h1); //this would append the created h1 element to the body
+container.appendChild(h1);
